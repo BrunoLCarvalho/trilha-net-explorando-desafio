d119626f4edec1fb7fc474fa63e82fd86f498b62
Bruno Carvalho <brunolourenco_2005@hotmail.com>
2024-04-24 19:10:38 -0300

    TODO concluido

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -17,17 +17,24 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+            try
             {
-                Hospedes = hospedes;
-            }
-            else
-            {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
-            }
-        }
+                if (hospedes.Count <= Suite.Capacidade)
+                {
+                    Hospedes = hospedes;
+                }
 
+                else
+                {
+                    throw new Exception("Capacidade do hotel excedida.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro ao cadastrar hóspedes: {ex.Message}");
+            }
+        
+        }
         public void CadastrarSuite(Suite suite)
         {
             Suite = suite;
@@ -37,24 +44,22 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valor = DiasReservados * Suite.ValorDiaria;   
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                Console.WriteLine("\n**Promoção baixa temporada, desconto de 10% aplicado com sucesso!**");
+                decimal valorDesconto = valor*0.10M;
+                Console.WriteLine($"Total desconto: R${valorDesconto}");
+                valor = valor - valorDesconto;
+                
             }
-
-            return valor;
+                return valor;
         }
     }
 }
